febc2b63753c01f1d90728b9e11badcd63a61fd5
GokulaKrishnan E <gokul.krish55@gmail.com>
2021-06-19 13:26:55 +0530

    Initial commit for RN_Fundamentals

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 
+
+/**
+ *1. Text=> SafeAreaView - we implemented SafeArea view to prevent the overlap text in top border
+ *2. Text=> NumberofLines - we added the NumberofLines props to display the long text in single remaining text is hidden
+ *3. Images=> 
+ */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Hello Gokul!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} style={styles.testStyle}> this is for testing the screen with large line of text to eanble it we need to add the number of lines props to enable to single line</Text>
+    </SafeAreaView>
   );
 }
 
@@ -13,7 +19,11 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
     backgroundColor: 'dodgerblue',
-    alignItems:'center',
-    justifyContent: 'center'
+  },
+  testStyle:{
+    fontSize:14,
+    padding:5,
+    margin:10
   }
 });
+
