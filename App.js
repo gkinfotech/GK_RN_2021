8d7dc8f91941c8e7a8b1b25b85d6c9eb330cff5b
GokulaKrishnan E <gokul.krish55@gmail.com>
2021-06-20 22:33:29 +0530

    WelcomeScreen and ViewImageScreen Initial commit

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.childContainer} />
-            <View style={styles.childContainer1} />
-            <View style={styles.childContainer2} />
-        </View>
-    );
+    return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    childContainer: {
-        flex: 1,
-        backgroundColor: 'dodgerblue',
-    },
-    childContainer1: {
-        flex: 1,
-        backgroundColor: 'gold',
-    },
-    childContainer2: {
-        flex: 1,
-        backgroundColor: 'tomato',
-    },
-});
